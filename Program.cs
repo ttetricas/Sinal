0f80a7f233c38257e8dfed4e3423ba59b4014cd6
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-03-28 21:28:53 -0300

    programa testado

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿int numero;
+
+Console.Write("Digite um número: ");
+numero = Convert.ToInt32(Console.ReadLine());
+
+if (numero < 0)
+{
+    Console.WriteLine ("Este é um número negativo!");
+}
+else if (numero > 0)
+{
+    Console.WriteLine ("Este é um número positivo!");
+}
+else
+{
+    Console.WriteLine ("Zero");
+}
